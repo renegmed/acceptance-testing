7ec14338a72cac02e755b5af2f982039a7944f92
r.medalla <r.medalla@us.nature.com>
2013-06-13 12:21:24 -0400

    rename main file

---
+++ src/main/java/com/nature/testing/AcceptanceTestRunner.java
@@ -0,0 +1,23 @@
+package com.nature.testing;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+import com.nature.testing.stepdefinitions.*;
+
+public class AcceptanceTestRunner {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	    Result result = JUnitCore.runClasses(
+	    		 ProductOntologyTest.class);
+	    
+	    for (Failure failure : result.getFailures()) {
+	      System.out.println(failure.toString());
+	    }		 
+
+	}
+
+}
